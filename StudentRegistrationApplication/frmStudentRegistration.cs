f6d77ed11efb19c1dcff1b4d85d41f00fb6d4eed
Venasdotcom <venasgiancarlo09@gmail.com>
2026-03-14 17:46:48 +0800

    update array list and foreach loops
    Added program selection to the student registration form and improved month selection logic.

--- StudentRegistrationApplication/frmStudentRegistration.cs
+++ StudentRegistrationApplication/frmStudentRegistration.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,34 +25,59 @@ namespace StudentRegistrationApplication
             cmbday.Items.Add("-Day-");
             cmbmonth.Items.Add("-Month-");
             cmbyear.Items.Add("-Year-");
+            cmbProg.Items.Add("-Select Program-");
 
 
 
-            for (int day = 1; day <= 31; day++)
+            ArrayList months = new ArrayList();
             {
-                cmbday.Items.Add(day);
+                months.Add("January");
+                months.Add("February");
+                months.Add("March");
+                months.Add("April");
+                months.Add("May");
+                months.Add("June");
+                months.Add("July");
+                months.Add("August");
+                months.Add("September");
+                months.Add("October");
+                months.Add("November");
+                months.Add("December");
+
             }
 
+                for (int day = 1; day <= 31; day++)
+                {
+                    cmbday.Items.Add(day);
+                }
 
-            for (int month = 1; month <= 12; month++)
-            {
-                cmbmonth.Items.Add(month);
+
+                foreach (string m in months)
+                {
+                    cmbmonth.Items.Add(m);
+                }
+
+
+                int currentYear = DateTime.Now.Year;
+                for (int year = 1900; year <= currentYear; year++)
+                {
+                    cmbyear.Items.Add(year);
+                }
+                ArrayList programs = new ArrayList();
+                programs.Add("Bachelor of Science in Computer Science");
+                programs.Add("Bachelor of Science in Information Technology");
+                programs.Add("Bachelor of Science in Tourism Management");
+                programs.Add("Bachelor of Science in Computer Engineering");
+                foreach (string p in programs)
+                {
+                    cmbProg.Items.Add(p);
+                }
+                cmbday.SelectedIndex = 0;
+                cmbmonth.SelectedIndex = 0;
+                cmbyear.SelectedIndex = 0;
+                cmbProg.SelectedIndex = 0;
+
             }
-
-
-            int currentYear = DateTime.Now.Year;
-            for (int year = 1900; year <= currentYear; year++)
-            {
-                cmbyear.Items.Add(year);
-            }
-
-
-            cmbday.SelectedIndex = 0;
-            cmbmonth.SelectedIndex = 0;
-            cmbyear.SelectedIndex = 0;
-        }
-        
-
         private void Register_Click(object sender, EventArgs e)
             
         {
@@ -106,14 +132,18 @@ namespace StudentRegistrationApplication
                     "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
-
+            if(cmbProg.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please select a program.",
+                    "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
             string day = cmbday.SelectedItem.ToString();
             string month = cmbmonth.SelectedItem.ToString();
             string year = cmbyear.SelectedItem.ToString();
-
             string gender = "";
             if (rbmale.Checked)
-            {
+            {   
                 gender = "Male";
             }
             else if (rbfemale.Checked)
@@ -122,19 +152,23 @@ namespace StudentRegistrationApplication
             }
 
             string fullName = fnamebox.Text + " " + mnamebox.Text + " " + lnamebox.Text;
-
-
             string dateOfBirth = day + "/" + month + "/" + year;
-
-
-            string message = "Student name: " + fullName + "\n"
-                           + "Gender: " + gender + "\n"
-                           + "Date of birth: " + dateOfBirth;
+            string message =
+                "Student name: " + fullName + "\n"
+                    +"Gender: " + gender + "\n"
+                       +"Date of birth: " + dateOfBirth+ "\n"
+                            +"Program: " + cmbProg.SelectedItem.ToString();
 
             MessageBox.Show(message);
         }
+
+        
+        
+
+        }
     }
-}
+    
+
 
 
 
